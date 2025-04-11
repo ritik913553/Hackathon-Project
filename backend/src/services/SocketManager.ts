ebdec8badb5af348412463e4e121cd419c8a0fbc
vivek201 <gvicky4567888@gmail.com>
2025-04-11 15:30:12 +0530

    socket setup done

--- backend/src/services/SocketManager.ts
+++ backend/src/services/SocketManager.ts
@@ -1,3 +1,38 @@
+import { IncomingMessage, Server, ServerResponse } from "http";
+import { RawData, WebSocket, WebSocketServer } from "ws";
+
 class SocketManager {
-    
+  private static _instance: SocketManager;
+  private wss: WebSocketServer | null = null;
+  private httpServer: Server<typeof IncomingMessage, typeof ServerResponse>;
+
+  constructor(server: Server<typeof IncomingMessage, typeof ServerResponse>) {
+    this.initSocket();
+    this.httpServer = server;
+  }
+
+  public static getInstance(
+    server: Server<typeof IncomingMessage, typeof ServerResponse>
+  ) {
+    if (this._instance) this._instance = new SocketManager(server);
+    return this._instance;
+  }
+
+  public initSocket() {
+    this.wss = new WebSocketServer({ server: this.httpServer });
+    this.wss?.on("connection", (ws: WebSocket) => {
+      this.handleSocketConnection(ws);
+    });
+  }
+
+  public handleSocketConnection(ws: WebSocket) {
+    ws.on("message", (data: RawData) => {
+      const message = JSON.parse(data.toString());
+
+      switch (message.type) {
+      }
+    });
+  }
 }
+
+export default SocketManager;
