2bb73fcc2e0317a5db12e4d8ae2d3d6ec3d37298
ritik913553 <ritikgupta9135@gmail.com>
2025-04-11 15:35:40 +0530

    add Like and Post models with schemas and interfaces

---
+++ backend/src/model/like.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema({
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    },
+    comment:{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },    
+    likedBy:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps:true})
+
+export const Like = mongoose.model("Like",likeSchema);
---
+++ backend/src/model/post.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        media: [
+            {
+                url: {
+                    type: String, // URL or path to the media file
+                    required: true,
+                },
+                type: {
+                    type: String, // Type of media: "image" or "video"
+                    enum: ["image", "video"],
+                    required: true,
+                },
+            },
+        ],
+        postDescription: {
+            type: String,
+            required: true,
+        },
+        tags: {
+            type: [String], // Tags for skills, technologies, or goals
+            default: [],
+        },
+        isMentorPost: {
+            type: Boolean, // True if the post is from a mentor
+            default: false,
+        },
+        comments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Comment", // Reference to the Comment schema
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", postSchema);
