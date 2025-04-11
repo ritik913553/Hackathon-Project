e0aa28cc8eab924f6c418d6c2129ec33efab80d2
ankitsaw <ankitsaw950@gmail.com>
2025-04-11 18:32:08 +0530

    feat: add dashboard route to routing configuration

--- frontend/src/utils/routes.tsx
+++ frontend/src/utils/routes.tsx
@@ -2,11 +2,13 @@ import { RouteObject } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Login from "../pages/Auth/Login";
 import Signup from "../pages/Auth/Signup";
+import Dashboard from "../pages/Dashboard";
 
 const routes: RouteObject[] = [
   { path: "/", element: <LandingPage /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup/> },
+  { path: "/dashboard", element: <Dashboard/> },
 ];
 
 export default routes;
