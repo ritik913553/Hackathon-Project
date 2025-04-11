d4ea0b62df92fd96c27c6a31c463d556676d8fd1
ankitsaw <ankitsaw950@gmail.com>
2025-04-11 17:35:41 +0530

    feat: add initial comments for frontend structure and navigation layout in About component

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -4,3 +4,5 @@ const About = () => {
   
   export default About;
   
+
+  // i have to make the frontend , this contains the landing page feature which tell about the app and the header and footer of this will be static for the auth part that is login and signup .  after that there will be a home page dashboard that contains the ui in a way that in the left of the website conatins the logo in the left topmost,and below their their will be actions like my groups,find mentors,become mentor,find project,theme button,logout.. after that by defaul the post section will apppear in the right side  and in that there will be a plca of creating a post and in below the existing post will appears,,  and when other actions will be clicked then their respective will appear just like post part..  simply create all this after that i will customise all the other pages and the components . every time the dashboard will be static and one more thing in the top righmost there will be home , notification,profile
