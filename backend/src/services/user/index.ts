55b2d89452ee375d91cc56e923c821cccce51703
ritik913553 <ritikgupta9135@gmail.com>
2025-04-11 15:57:32 +0530

    implement getUserByEmail function

---
+++ backend/src/services/user/index.ts
@@ -0,0 +1,21 @@
+import User from "../../model/user.model";
+export const getUserByEmail = (email: string) => {
+    try {
+        const user = User.findOne({ email });
+        if (!user) {
+            return {
+                success: false,
+                message: "User not found",
+            };
+        }
+        return {
+            success: true,
+            user,
+        };
+    } catch (error) {
+        return {
+            success: false,
+            message: "Internal server error",
+        };
+    }
+};
